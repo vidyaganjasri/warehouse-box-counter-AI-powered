b76f448b487f3ef1c61ee91a60159494c034cf23
Ganja Srividya <134138048+vidyaganjasri@users.noreply.github.com>
2025-05-17 01:20:33 +0530

    Update test.py

--- yolov8/test.py
+++ yolov8/test.py
@@ -1,4 +1,14 @@
-from ultralytics import YOLO
-model = YOLO("yolov8m_custom.pt")
+import sys
+import os
+sys.path.append(os.path.abspath('../WEBCAM_CAPTURE'))
 
-model.predict(source='warehouse.jpg',show=True,save=True,conf=0.85,line_thickness=1)
+from webcam_capture import main as get_input_file
+from ultralytics import YOLO
+
+image_path = get_input_file()
+if image_path is None:
+    print("❌ No image was captured.")
+    exit()
+
+model = YOLO("yolov8m_custom.pt")
+model.predict(source=image_path, show=True, save=True, conf=0.85, line_thickness=1)
